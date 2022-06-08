5915fb4984782c85e9eb2f11121c8b66edce0512
Jordan Minota <popjj@unimayor.edu.co>
2022-06-08 10:35:50 -0500

    fix-responsive-320

--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f4da6678bdb65a36"; }
+/******/ 		__webpack_require__.h = function() { return "9e1b06cf5c974890"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
